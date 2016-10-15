e6c1ebcf9419bab0fca72705dcfa36ed2b4e6ebb
Tan Si Nan <tansinan_1995@163.com>
2016-10-15 20:28:32 +0800

    Implement size exponential to linear file size extending.

--- Storage/DiskStorageArea.cpp
+++ Storage/DiskStorageArea.cpp
@@ -1,9 +1,11 @@
 #include <boost/filesystem.hpp>
+#include <boost/log/trivial.hpp>
 #include "DiskStorageArea.h"
 
 DiskStorageArea::DiskStorageArea(std::string fileName)
 {
-    boost::filesystem::path filePath = fileName;
+    this->fileName = fileName;
+    filePath = fileName;
     if (!boost::filesystem::is_regular_file(filePath))
     {
         fstream.open(fileName, std::ios::trunc|std::ios::out);
@@ -13,22 +15,24 @@ DiskStorageArea::DiskStorageArea(std::string fileName)
             return;
         }
     }
-    if(boost::filesystem::file_size(filePath) < FILE_SIZE)
+    if(boost::filesystem::file_size(filePath) < SIZE_INITITAL)
     {
         boost::system::error_code err;
-        boost::filesystem::resize_file(filePath, FILE_SIZE, err);
+        boost::filesystem::resize_file(filePath, SIZE_INITITAL, err);
         if(err != boost::system::errc::success)
         {
+            BOOST_LOG_TRIVIAL(error) << "File size allocation failed!";
             return;
         }
     }
-    fstream.open(fileName);
+    currentSize = boost::filesystem::file_size(filePath);
+    fstream.open(fileName, std::ios::in | std::ios::out | std::ios::binary);
     valid = fstream.is_open();
 }
 
 uint64_t DiskStorageArea::getSize()
 {
-    return FILE_SIZE;
+    return currentSize;
 }
 
 bool DiskStorageArea::isValid()
@@ -36,16 +40,54 @@ bool DiskStorageArea::isValid()
     return valid;
 }
 
+bool DiskStorageArea::checkOffset(uint64_t offset)
+{
+    if(offset < currentSize)
+    {
+        return true;
+    }
+    while(offset >= currentSize && currentSize < SIZE_EXPONENTIAL_GROW_LIMIT)
+    {
+        currentSize *= SIZE_GROW_RATIO;
+    }
+    while(offset >= currentSize)
+    {
+        currentSize += SIZE_GROW_BYTES;
+    }
+    BOOST_LOG_TRIVIAL(debug) <<
+        "Trying to extend file " << fileName << " to " <<
+        currentSize / 1024 / 1024  << " MB.";
+    fstream.close();
+    valid = false;
+    boost::system::error_code err;
+    boost::filesystem::resize_file(filePath, currentSize, err);
+    if(err != boost::system::errc::success)
+    {
+        BOOST_LOG_TRIVIAL(error) << "File extending failed!";
+        return false;
+    }
+    fstream.open(fileName, std::ios::in | std::ios::out | std::ios::binary);
+    valid = fstream.is_open();
+    if(!valid)
+    {
+        BOOST_LOG_TRIVIAL(error) << "File extending failed!";
+    }
+    return valid;
+}
+
 bool DiskStorageArea::setDataAt(int offset, char* data, int length)
 {
+    checkOffset(offset + length);
     fstream.seekp(offset, std::ios_base::beg);
     fstream.write(data, length);
+    fstream.flush();
     //TODO: error handling
     return true;
 }
 
 bool DiskStorageArea::getDataAt(int offset, char* data, int length)
 {
+    checkOffset(offset + length);
     fstream.seekg(offset, std::ios_base::beg);
     fstream.read(data, length);
     //TODO: error handling
--- Storage/DiskStorageArea.h
+++ Storage/DiskStorageArea.h
@@ -7,12 +7,28 @@ class DiskStorageArea : public AbstractStorageArea
 protected:
     bool valid = false;
     std::string fileName;
+    boost::filesystem::path filePath;
     std::fstream fstream;
-    const int FILE_SIZE = 1024 * 1024 * 1024;
+
+    // The current size for fstream object.
+    int currentSize = 0;
+
+    // The initial size of data file (16MB)
+    const int SIZE_INITITAL = 16 * 1024 * 1024;
+
+    // The ratio for exponential size growing (x2)
+    const int SIZE_GROW_RATIO = 2;
+
+    // The size to increase for increaasing data access (256MB)
+    const int SIZE_GROW_BYTES = 256 * 1024 * 1024;
+
+    // Max size for exponential size growing (512M)
+    const int SIZE_EXPONENTIAL_GROW_LIMIT = 512 * 1024 * 1024;
 public:
     DiskStorageArea(std::string fileName);
     virtual bool isValid();
     virtual uint64_t DiskStorageArea::getSize();
+    virtual bool checkOffset(uint64_t offset);
     virtual bool setDataAt(int offset, char* data, int length);
     virtual bool getDataAt(int offset, char* data, int length);
     virtual bool close();
