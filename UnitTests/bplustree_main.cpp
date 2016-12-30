c294fcc8dd1218bb08db4f0331afce793596307e
Tan Si Nan <tansinan_1995@163.com>
2016-12-30 20:58:00 +0800

    Update B+ tree unit test.

--- UnitTests/bplustree_main.cpp
+++ UnitTests/bplustree_main.cpp
@@ -19,13 +19,12 @@
 #include "FieldTypes/SmallIntFieldType.h"
 #include "FieldTypes/CharacterFieldType.h"
 #include "Index/BPlusTree.h"
-
-int main(int argc, char* argv[])
+void test_bpt(int argc, char* argv[])
 {
-    srand(123);
+    srand(124);
     std::string fileName = "D:/data.raw";
     //DiskStorageArea storageArea(fileName);
-    CachedStorageArea storageArea(fileName, 8 * 1024 * 1024, 8192);
+    CachedStorageArea storageArea(fileName, 16 * 1024 * 1024, 8192);
     BlockAllocator blockAllocator(&storageArea);
     blockAllocator.initialize();
     uint64_t block = blockAllocator.allocate().getOffset();
@@ -33,37 +32,86 @@ int main(int argc, char* argv[])
     BPlusTree::Node rootNode(&bpt, block);
     rootNode.initialize();
     bpt.root = rootNode;
-    int arr[65536];
-    for (int i = 0; i < 65536; i++)
+    const int TEST_DATA_COUNT = 500000;
+    const int TEST_COUNT = TEST_DATA_COUNT / 2;
+    int* arr = new int[TEST_DATA_COUNT];
+    for (int i = 0; i < TEST_DATA_COUNT; i++)
     {
         arr[i] = i;
     }
-    for (int i = 0; i < 65536 * 8; i++)
+    for (int i = 0; i < TEST_DATA_COUNT * 8; i++)
     {
-        int p = rand() % 65536;
-        int q = rand() % 65536;
+        int p = (rand() * rand()) % TEST_DATA_COUNT;
+        int q = (rand() * rand()) % TEST_DATA_COUNT;
         if (p != q) std::swap(arr[p], arr[q]);
     }
-    const int TEST_COUNT = 65536 / 2;
+    std::cout << "Data generated." << std::endl;
+    std::cout << "Testing insert 1." << std::endl;
     for (int i = 0; i < TEST_COUNT; i++)
     {
-        if (i % 100 == 0) std::cout << i << std::endl;
-        bpt.insert(arr[i], &bpt.root);
+        bpt.insert(arr[i]);
     }
-    for (int i = 0; i < 65536; i++)
+    for (int i = 0; i < TEST_COUNT; i++)
     {
-        int Search(int x, BPlusTree::Node *root);
-        //std::cout << i << std::endl;
-        if (Search(arr[i], &bpt.root) == 1 && i >= TEST_COUNT)
+        int key = arr[i];
+        if (bpt.search(key) == 0 && i < TEST_COUNT)
         {
-            std::cout << "Error " << i << " " << arr[i] << std::endl;
+            std::cout << "Error" << std::endl;
         }
-        else if(Search(arr[i], &bpt.root) == 0 && i < TEST_COUNT)
+        else if (bpt.search(key) == 1 && i >= TEST_COUNT)
         {
-            std::cout << "Error " << i << " " << arr[i] << std::endl;
+            std::cout << "Error" << std::endl;
         }
     }
-    std::cout << "Done." << std::endl;
-    std::cout << bpt.root.address << std::endl;
-    return 0;
+    std::cout << "Testing insert 2." << std::endl;
+    for (int i = TEST_COUNT; i < TEST_DATA_COUNT; i++)
+    {
+        bpt.insert(arr[i]);
+    }
+    for (int i = 0; i < TEST_DATA_COUNT; i++)
+    {
+        int key = arr[i];
+        if (bpt.search(key) == 0)
+        {
+            std::cout << "Error" << std::endl;
+        }
+    }
+    std::cout << "Testing delete 1." << std::endl;
+    for (int i = 0; i < TEST_COUNT; i++)
+    {
+        bpt.Delete(arr[i]);
+    }
+    for (int i = 0; i < TEST_COUNT; i++)
+    {
+        int key = arr[i];
+        if (bpt.search(key) == 1 && i < TEST_COUNT)
+        {
+            std::cout << "Error1" << std::endl;
+        }
+        else if (bpt.search(key) == 0 && i >= TEST_COUNT)
+        {
+            std::cout << "Error2" << std::endl;
+        }
+    }
+    std::cout << "Testing delete 2." << std::endl;
+    for (int i = TEST_COUNT; i < TEST_DATA_COUNT; i++)
+    {
+        bpt.Delete(arr[i]);
+    }
+    for (int i = 0; i < TEST_DATA_COUNT; i++)
+    {
+        int key = arr[i];
+        if (bpt.search(key) == 1)
+        {
+            std::cout << "Error1" << std::endl;
+        }
+    }
+    if (bpt.root.getIsLeaf() && bpt.root.getUsedKeyCount() == 0)
+    {
+        std::cout << "OK." << std::endl;
+    }
+    else
+    {
+        std::cout << "Error." << std::endl;
+    }
 }
